3127f9e0eb26bc9820bf5315c577c94901f38307
heywatsupsmo-create <heywatsupsmo@gmail.com>
2026-03-24 21:54:56 -0400

    Refactor database connection and registration logic
    Updated database connection to use environment variables and SSL.

--- connect.php
+++ connect.php
@@ -1,18 +1,29 @@
 <?php
-	$first = $_POST['first'];
-	$password = $_POST['password'];
-	
-	//Database Connection
-	$conn = new mysqli('localhost', 'root', '', 'test');
-	if($conn->connect_error) {
-    die('Connection Failed : ' . $conn->connect_error); // Corrected line
-	} else {
-		$stmt = $conn->prepare("insert into registration (first, password)
-			values(?,?)");
-		$stmt->bind_param("ss",$first, $password);
-		$stmt->execute();
-		echo "registration Successfully";
-		$stmt-close();
-		$conn->close();
-	}
+$first = $_POST['first']; 
+$password = $_POST['password']; 
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+$conn = mysqli_init();
+$conn->ssl_set(NULL, NULL, "/var/www/html/ca.pem", NULL, NULL);
+
+$success = $conn->real_connect($host, $user, $pass, $db, $port);
+
+if (!$success) {
+    die('Connection Failed : ' . mysqli_connect_error());
+} else {
+    $stmt = $conn->prepare("INSERT INTO registration (first, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $first, $password);
+    
+    if($stmt->execute()) {
+        echo "Registration Successful!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    $stmt->close();
+    $conn->close();
+}
 ?>
